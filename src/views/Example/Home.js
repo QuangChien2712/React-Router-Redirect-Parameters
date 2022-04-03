b69feead899b5f1ef977a83b94974ee87182c340
Chien <cvoquang@gmail.com>
2022-04-03 22:35:55 +0700

    first commit

---
+++ src/views/Example/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { withRouter } from "react-router";
+import Color from "../HOC/Color";
+
+class Home extends React.Component {
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.props.history.push('/todo')
+        // }, 3000)
+    }
+
+    //HOC: higher order component
+    render() {
+        console.log(">>> check props: ", this.props);
+        return <div > Hello world from Homepage < /div>;
+    }
+}
+
+// export default withRouter(Home);
+export default Color(Home);
